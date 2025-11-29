c5b580530b4713d68f1a4cb8b0debd8b6a1e04e7
Nate Martinez <nrd.martinezz@gmail.com>
2025-11-28 17:06:54 -0800

    fixes debugger

--- coreboost.php
+++ coreboost.php
@@ -23,6 +23,7 @@ if (!defined('ABSPATH')) {
 
 // Define plugin constants
 define('COREBOOST_VERSION', '2.5.0');
+
 define('COREBOOST_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('COREBOOST_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('COREBOOST_PLUGIN_BASENAME', plugin_basename(__FILE__));
